20d631c3a407261b1c7325199f58e571e3fd9556
abdul-hasan <abdalamohamedhaazzan@gmail.com>
2026-01-31 17:24:50 +0300

    added home page.

--- dynamic.js
+++ dynamic.js
@@ -2,17 +2,21 @@
 let menu = document.getElementById("menu");
 let icons = menu.querySelectorAll("i");
 let navigationBar = document.getElementById("navigation");
+// program starts whene ever user clicks the menu burger, either bars or close icons//
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", ()=> {
+    // every time user clicked the menu element run this function//
     let currentState = navigationBar.getAttribute("appear");
+    // do checking//
     if(currentState === "true"){
         icons[0].setAttribute("appear", "true");
         icons[1].setAttribute("appear", "false");
-        navigationBar.setAttribute("appear", "true");
+        navigationBar.setAttribute("appear", "false");
+
     }else{
         icons[0].setAttribute("appear", "false");
         icons[1].setAttribute("appear", "true");
-        navigationBar.setAttribute("appear", "false"); 
+        navigationBar.setAttribute("appear", "true");
     }
 })
 
